3f540533f4249f328f9b233db556ac4b26957f32
MrAlphaF <mralphaf@gmail.com>
2024-10-01 16:37:55 +0300

    Update 2.2.1_Arturs_Silins.py

--- PracticalTasks/PracticalTask2/solutions/2.2.1_Arturs_Silins.py
+++ PracticalTasks/PracticalTask2/solutions/2.2.1_Arturs_Silins.py
@@ -5,9 +5,10 @@ def draw_star(n, lenght):
 
     angle = 180-(180/n)
     for _ in range(n):
+        t.speed(0)
         t.forward(lenght)  
         t.right(angle)    
 
     turtle.done()
     
-draw_star(13,400)
+draw_star(101,400)
